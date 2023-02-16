a14253273800ac8368f737d5d5c1f1c4e6e533ca
chingyi lan <jinny8813@gmail.com>
2023-02-17 05:34:12 +0800

    小修測驗，選擇字卡狀態

--- app/Models/QuizModel.php
+++ app/Models/QuizModel.php
@@ -10,6 +10,23 @@ class QuizModel extends Model
     protected $allowedFields = ['quiz_id','user_id','select_book','select_old','select_wrong','select_state','select_amount','add_random','quiz_list','create_at'];
 
     public function getNewQuiz($book_id,$select_old,$select_wrong,$select_state,$select_amount){
+        switch ($select_state){
+            case "差":
+                $state=[1,2];
+                break;
+            case "弱":
+                $state=[3,4];
+                break;
+            case "中":
+                $state=[5,6];
+                break;
+            case "可":
+                $state=[7,8];
+                break;
+            case "佳":
+                $state=[9,10];
+                break;
+        }
         $db = \Config\Database::connect();
         $builder = $db->table('cards c');
         if($select_state=="未測驗"){
@@ -27,6 +44,7 @@ class QuizModel extends Model
                     ->join('eventlog e','c.card_id = e.card_id')
                     ->where('DATEDIFF(CURDATE(),DATE(c.create_at))>=', $select_old)
                     ->where('c.book_id', $book_id)
+                    ->whereIn('c.card_state', [0])
                     ->whereIn('e.choose', ["模糊","忘記"])
                     ->where('DATEDIFF(CURDATE(),DATE(e.create_at))<=', 30)
                     ->groupBy('c.card_id')
@@ -40,6 +58,7 @@ class QuizModel extends Model
                 $query = $builder->select("c.card_id")
                     ->join('eventlog e','c.card_id = e.card_id')
                     ->where('c.book_id', $book_id)
+                    ->whereIn('c.card_state', $state)
                     ->where('DATEDIFF(CURDATE(),DATE(e.create_at))>=', $select_old)
                     ->groupBy('c.card_id')
                     ->orderBy('title', 'RANDOM')
@@ -50,6 +69,7 @@ class QuizModel extends Model
                     ->join('eventlog e','c.card_id = e.card_id')
                     ->where('DATEDIFF(CURDATE(),DATE(e.create_at))>=', $select_old)
                     ->where('c.book_id', $book_id)
+                    ->whereIn('c.card_state', $state)
                     ->whereIn('e.choose', ["模糊","忘記"])
                     ->where('DATEDIFF(CURDATE(),DATE(e.create_at))<=', 30)
                     ->groupBy('c.card_id')
