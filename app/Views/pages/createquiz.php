99d84f7f00ff17012dedd7646308f3046e885839
chingyi lan <jinny8813@gmail.com>
2023-03-06 02:56:11 +0800

    測驗數暫定50以下，100以上好像存不到資料

--- app/Views/pages/createquiz.php
+++ app/Views/pages/createquiz.php
@@ -72,9 +72,6 @@
                             <option value="10">10</option>
                             <option value="20">20</option>
                             <option value="50">50</option>
-                            <option value="100">100</option>
-                            <option value="200">200</option>
-                            <option value="500">500</option>
                         </select>
                     </div>
                 </div>
